47c65c191a2debced0f0b771c5281a964928dd43
Lorenzo Cameroni <lorenzo.cameroni@gmail.com>
2020-12-18 14:44:35 +0100

    Aggiunti commenti

--- model/model.go
+++ model/model.go
@@ -6,10 +6,12 @@ import (
 	"time"
 )
 
+// Image rappresenta un immagine con cartella, nome file e data
 type Image struct {
 	BasePath string
 	FileName string
 	Time     time.Time
 }
 
+// Logger per tutto il programma
 var Logger = log.New(os.Stdout, "photoorganizer: ", log.Lshortfile)
